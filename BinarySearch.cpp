e6faf8788d68799e63c097660e1985ec1f7c3ab1
Prabin Pramanik <thisIsPrabin@users.noreply.github.com>
2016-07-10 03:11:30 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,55 +5,59 @@
 #include<iostream>
 using namespace std;
 
-int binarySearch(int array[], int left, int right, int key){
-	
-	int i, mid;
-	mid = (right+1) / 2;
-	
-	if(array[mid] == key){
-		return mid;
-	}
-	
-	if(array[mid] > key){
+class BinarySearch {
+
+	public:	
 		
-		for(i=left; i<mid; i++){
+	int search(int array[], int left, int right, int key){
+		
+		int i, mid;
+		mid = (right+1) / 2;
+		
+		if(array[mid] == key){
+			return mid;
+		}
+		
+		if(array[mid] > key){
+			
+			for(i=left; i<mid; i++){
+				if(array[i] == key){
+					break;
+				}
+			}
+			
 			if(array[i] == key){
-				break;
+				return i;
+			} else {
+				return -1;
 			}
 		}
 		
-		if(array[i] == key){
-			return i;
-		} else {
-			return -1;
-		}
-	}
-	
-	if(array[mid] < key){
-		
-		for(i=mid; i<=right; i++){
+		if(array[mid] < key){
+			
+			for(i=mid; i<=right; i++){
+				if(array[i] == key){
+					break;
+				}
+			}
+			
 			if(array[i] == key){
-				break;
+				return i;
+			} else {
+				return -1;
 			}
 		}
 		
-		if(array[i] == key){
-			return i;
-		} else {
-			return -1;
-		}
+		return -1;
 	}
-	
-	return -1;
-}
+};
 
 int main(){
 	
-	int ack;
 	int array[5] = {1, 2, 3, 4, 5};
 	
-	ack = binarySearch(array, 0, 4, 5);
+	BinarySearch obj;
+	cout << obj.search(array, 0, 4, 5);
 	
-	cout << ack;
 	return 0;
 }
