ea89d1d9e7a729bb6db0130e488d2609b9feb0d3
Prabin Pramanik <thisIsPrabin@users.noreply.github.com>
2016-07-16 04:22:02 +0530

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class InsertionSort {
+
+	public:
+
+	void sort(int array[], int length){
+		
+		int temp;
+		
+		for(int i=0; i<=length-1; i++){
+			for(int j=i; j>=0; j--){
+				
+				if(array[j-1] > array[j]){
+					temp = array[j];
+					array[j] = array[j-1];
+					array[j-1] = temp;
+				}
+			}
+		}
+			
+		for(int i=0; i<length; i++){
+			printf("%d\t", array[i]);
+		}
+	}
+};
+
+
+int main(){
+	
+	int array[] = {98, 1, 45, 38, 22};
+	int length = sizeof(array) / sizeof(array[0]);
+	
+	InsertionSort obj;
+	obj.sort(array, length);
+	
+	return 0;
+}
